eb88703109229e4e5a4b568bda7410c40494ddbc
wthe22 <>
2023-01-08 20:02:32 +0700

    Magic-Number-Card-3R1 1.1

--- Magic-Number-Card-3R1.bat
+++ Magic-Number-Card-3R1.bat
@@ -1,104 +1,154 @@
+:entry_point
+@goto main
+
+
+:changelog
+::  v1.1 (2023-01-08)
+::  - Fix program showing incorrect guess after the user choose to play again
+::  - Reworked
+::
+::  v1.0 (2012-09-30)
+::  - First release
+exit /b 0
+
+
+:metadata [return_prefix]
+set "%~1name=mnc3.1"
+set "%~1version=1.1"
+set "%~1authors=wthe22"
+set "%~1license=The MIT License"
+set "%~1description=Magic Number Card 3R1"
+set "%~1release_date=01/08/2021"   :: mm/dd/YYYY
+set "%~1url=https://github.com/wthe22/batchlib"
+set "%~1download_url=https://raw.githubusercontent.com/wthe22/batch-scripts/master/Magic-Number-Card-3R1.bat"
+exit /b 0
+
+
+:config
+set "guess_max=95"
+set "symbols= XY"
+exit /b 0
+
+
+:main
+@setlocal EnableDelayedExpansion EnableExtensions
 @echo off
-setlocal EnableDelayedExpansion
+call :metadata SOFTWARE.
+call :config
+call :play
+exit /b 0
 
 
-rem Settings
-set "guessMaxNum=95"
-set "sym1=X"
-set "sym2=Y"
-rem ====================
+:play
+set "guess_number=0"
+set "card_number=1"
+set "card_power=1"
 
-set "guessNumber=0"
-set "cardNumber=1"
-set "cardPower=1"
-
-:setup
 cls
-title Magic Number Card 3.1
-echo Think of a number from 1 to %guessMaxNum%
+echo !SOFTWARE.description!
+echo=
+echo Think of a number from 1 to !guess_max!
 echo Let me guess your number
 pause
 
-:genMNC
-cls
-title Magic Number Card 3.1 #%cardNumber%
-echo Generating the card...
+call :play.answer
+for /f "tokens=* delims=0" %%a in ("!code!") do set "code=%%a"
+set "error="
+if !guess_number! LSS 1 set "error=true"
+if !guess_number! GTR !guess_max! set "error=true"
 
-set "card="
-set "symNum=1"
-set /a nextNumber=%cardPower%/2 + %cardPower% %% 2
-set "loop1=1"
-:genCard
-set "display=    !sym%symNum%!%nextNumber%"
-set "card=%card%%display:~-5,5%"
-:genNum
-if %loop1% == %cardPower% goto loop1Reset
-set /a loop1+=1
-goto genNumDone
-:loop1Reset
-set "loop1=1"
-set /a symNum+=1
-if %symNum% == 3 goto loop2Reset
-goto genNumDone
-:loop2Reset
-set "symNum=1"
-set /a nextNumber+=%cardPower%
-goto genNumDone
-:genNumDone
-set /a nextNumber+=1
-if %nextNumber% GTR %guessMaxNum% goto displayMNC
-goto genCard
 
-:displayMNC
 cls
-echo %card%
+echo !SOFTWARE.description!
+echo=
+if defined error (
+    echo There is something wrong with the guessing
+    echo=
+    echo Your number should be between 1 - !guess_max!
+    echo Or you should find the number and answer more carefully next time
+) else (
+    echo Your number  : !guess_number!
+    echo Guessing code: !code!
+)
+echo=
+choice /c YN /m "Do you want to play again?"
+set "choice=!errorlevel!"
+if "!choice!" == "1" goto play
+exit /b 0
+
+
+:play.answer
+if !card_power! GTR !guess_max! exit /b 0
+cls
+echo !SOFTWARE.description! #!card_number!
+echo=
+call :generate_mnc card !card_power! !guess_max!
+call :format_size card 5
+call :play.answer.show
+set /a "card_power*=3"
+goto play.answer
+
+
+:play.answer.show
+cls
+echo !card!
 echo=
 choice /c:YN /m:"Is your number there?"
-set "choice=%errorlevel%"
-if %choice%==2 set "guessNumber_Code=0%guessNumber_Code%" & goto nextMNC
-choice /c:%sym1%%sym2% /m:"What symbol is it?"
-set "choice=%errorlevel%"
-if %choice%==1 set "guessNumber_Code=1%guessNumber_Code%" & set /a guessNumber+=%cardPower%
-if %choice%==2 set "guessNumber_Code=2%guessNumber_Code%" & set /a guessNumber-=%cardPower%
-:nextMNC
-set /a cardNumber+=1
-set /a cardPower*=3
-if %cardPower% gtr %guessMaxNum% goto setupResult
-goto genMNC
+set "choice=!errorlevel!"
+if "!choice!" == "2" (
+    set "code=0!code!"
+    exit /b 0
+)
+choice /c !symbols! /m "What symbol is it?"
+set "choice=!errorlevel!"
+set "code=!choice!!code!"
+if "!choice!" == "1" set /a "guess_number+=!card_power!"
+if "!choice!" == "2" set /a "guess_number-=!card_power!"
+exit /b 0
 
-:setupResult
-title Magic Number Card 3R.X
-if %guessNumber% lss 1 goto resultError
-if %guessNumber% gtr %guessMaxNum% goto resultError
 
-:codeKill0
-if %guessNumber_Code:~0,1%==0 set "guessNumber_Code=%guessNumber_Code:~1%" & goto codeKill0
+:generate_mnc <return_var> <power> <max>
+set "_return_var=%~1"
+set "_power=%~2"
+set "_max=%~3"
+set "_result="
+set /a "next_number=!_power!/2 + 1"
+call :generate_mnc._loop
+for /f "tokens=* delims=" %%r in ("!_result!") do (
+    endlocal
+    set "%_return_var%=%%r"
+)
+exit /b 0
+:generate_mnc._loop
+for /l %%s in (1,1,2) do (
+    for /l %%j in (1,1,!_power!) do (
+        if !next_number! GTR !_max! exit /b 0
+        set "_result=!_result! !symbols:~%%s,1!!next_number!"
+        set /a "next_number+=1"
+    )
+)
+set /a "next_number+=!_power!"
+goto generate_mnc._loop
 
-cls
-echo Your number  : %guessNumber%
-echo Guessing code: %guessNumber_Code%
-echo=
-pause
-goto pPlayAgain
 
-:resultError
-cls
-echo Your number  : %guessNumber%
-echo There is something wrong with the guessing
-echo Your answers show that your number is not in any cards
-echo Your number is also not between 1 - %guessMaxNum%
-echo=
-echo Your number should be between 1 - %guessMaxNum%
-echo Or you should find the number more carefully next time
-echo=
-pause
-goto pPlayAgain
+:format_size <input_var> <size>
+set "_input_var=%~1"
+set "_size=%~2"
+set "_spaces="
+for /l %%n in (1,1,!_size!) do set "_spaces=!_spaces! "
+set "_result="
+for %%v in (!%_input_var%!) do (
+    set "_value=!_spaces!%%v"
+    set "_result=!_result!!_value:~-%_size%,%_size%!"
+)
+for /f "tokens=* delims=" %%r in ("!_result!") do (
+    endlocal
+    set "%_input_var%=%%r"
+)
+exit /b 0
 
-:pPlayAgain
-cls
-choice /c:YN /m:"Do you want to play again?"
-if %errorlevel%==1 goto setup
-exit
+
+
 
    X1   Y2   X4   Y5   X7   Y8  X10  Y11  X13  Y14  X16  Y17  X19  Y20  X22  Y23
   X25  Y26  X28  Y29  X31  Y32  X34  Y35  X37  Y38  X40  Y41  X43  Y44  X46  Y47
